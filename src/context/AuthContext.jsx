dfce443746d65c2881b74e4bd6dc1700ea24936c
Muhamad Gibran <49028961+M-Gibran@users.noreply.github.com>
2021-01-08 20:47:31 +0700

    Axios setup

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -7,8 +7,25 @@ const authReducer = (state, action) => {
   }
 };
 
+const signup = (dispatch) => {
+  return ({ email, password }) => {};
+};
+
+const signin = (dispatch) => {
+  return ({ email, password }) => {
+    // Try to signim
+    // handle succes by updating state
+    // handle failure by showing error message
+  };
+};
+
+const signout = (dispatch) => {
+  return () => {
+    // sign put
+  };
+};
 export const { Provider, Context } = createDataContext(
   authReducer,
-  {},
+  { signin, signup, signout },
   { isSignedIn: false }
 );
